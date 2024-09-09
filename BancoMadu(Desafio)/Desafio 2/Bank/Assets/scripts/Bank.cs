143d06995e0335ab43e7910f276eebf63efde137
MaduPereira <dudasilvapereira@outlook.com.br>
2024-09-09 08:58:19 -0300

    update

---
+++ BancoMadu(Desafio)/Desafio 2/Bank/Assets/scripts/Bank.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //Importando a biblioteca de gerenciamento das Scenes in Build
+using UnityEngine.UI; //Importando a biblioteca dos objetos
+using System.Text.RegularExpressions; //Importando a biblioteca do texto
+using System;
+
+//Aluna: Madu
+//Data:09/05/2022
+//Objetivo: Fazer as operações do banco de saque e depósito; Mostrar na interface o meu usuário cadastrado; Apresentar na Interface o feedback;
+
+public class Bank : MonoBehaviour
+{
+    public static string caixauser; //variável que vai receber o usuário de uma variavel static string
+
+    public GameObject Saldo; //declarando objeto em cena que vai ser conectados
+    public GameObject Valor; //declarando objeto em cena que vai ser conectados
+    public GameObject Pessoa; //declarando objeto em cena que vai ser conectado ao caixauser
+
+    private float valor; //variável para receber meu GameObject Valor
+    private  float saldo; //variável para receber a variável valor
+
+    public GameObject Aviso; //caixinha de texto avisando o tempo de inatividade //declarando objetos em cena que vão ser conectados
+
+    public static string saque;
+    public static string depo;
+
+    public static string saldinho;
+
+    public static List<string> operacoes = new List<string>();
+    public static List<float> TotalSaldo = new List<float>();
+
+    public void sacar(){ //método para efetuar o saque
+        
+        
+        valor = float.Parse(Valor.GetComponent<InputField>().text); //usei o float.Parse para transformar o Valor de string para float possibilitando as operações 
+        if(valor <= saldo && valor > 0){ //fazendo o tratamento de erro
+
+            TotalSaldo[Login.Index] -= valor;
+            Debug.Log(TotalSaldo[0] + TotalSaldo[1]);
+            saldo -= valor;
+            Debug.Log("voce sacou " +Valor.transform.Find("Text").gameObject.GetComponent<Text>().text); //teste
+            Saldo.GetComponent<InputField>().text = saldo.ToString(); //usei o .ToString para transformar saldo em string possibilitando o Input Saldo exibir os valores 
+            saldinho = Saldo.GetComponent<InputField>().text;
+            saque = Valor.GetComponent<InputField>().text;
+            operacoes.Add("-" + saque);
+            Debug.Log(operacoes);
+            int i = Registrar.usuario.LastIndexOf(""); 
+            if(Registrar.usuario[i] == "")
+            {
+                Registrar.usuario.RemoveAt(i);
+            }
+
+        }    
+
+    }
+
+    public void depositar(){ //método para efetuar o depósito
+        
+        valor = float.Parse(Valor.GetComponent<InputField>().text);//usei o float.Parse para transformar o Valor de string para float possibilitando as operações 
+        if(valor > 0){     //fazendo o tratamento de erro
+            saldo += valor;
+            Debug.Log("vc depositou " +Valor.transform.Find("Text").gameObject.GetComponent<Text>().text); //teste
+            Saldo.GetComponent<InputField>().text = saldo.ToString(); //usei o .ToString para transformar saldo em string possibilitando o Input Saldo exibir os valores
+            saldinho = Saldo.GetComponent<InputField>().text;
+            depo = Valor.GetComponent<InputField>().text;
+            operacoes.Add("+" + depo);
+            Debug.Log(operacoes);
+            int i = Registrar.usuario.LastIndexOf(""); 
+            if(Registrar.usuario[i] == "")
+            {
+                Registrar.usuario.RemoveAt(i);
+            }
+
+        }
+    }
+
+    IEnumerator IniciarCoroutine() //método para fazer um timer
+    {
+        yield return new WaitForSeconds(5); //definindo os segundos
+        Aviso.SetActive(true); //para ativar a caixa de texto
+        yield return new WaitForSeconds(10); //definindo os segundos
+        SceneManager.LoadScene("Inicio");
+
+    }
+
+    public void umacliclada()  //método para resetar o time já feito se o usuário reclicou na tela
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            StopCoroutine("IniciarCoroutine"); //pause no tempo
+            Aviso.SetActive(false);
+            StartCoroutine("IniciarCoroutine"); //star no tempo
+        }
+    }
+
+    public void extra()
+    {
+        SceneManager.LoadScene("Extrato");
+    }
+    
+
+
+    void Update()
+    {
+        umacliclada(); //aqui eu starto o meu método de tempo
+        caixauser=Login.usuarioReceb; //aqui no update pois o user pode ser alterado com um novo cadastro
+        Pessoa.GetComponent<Text>().text = caixauser;
+        
+    }
+
+    void Start()
+    {
+        Saldo.GetComponent<InputField>().text = saldinho;
+    }
+}
---
+++ BancoMadu(Desafio)/Desafio 2/Bank/Assets/scripts/Login.cs
@@ -0,0 +1,84 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //Importando a biblioteca dos objetos
+using UnityEngine.SceneManagement; //Importando a biblioteca de gerenciamento das Scenes in Build
+
+//Aluna: Madu
+//Data:09/05/2022
+//Objetivo: Fazer a validação do login de um usuário cadastrado; Apresentar na Interface o feedback;
+
+public class Login : MonoBehaviour
+{
+    string user; //variável declarada para receber o valor de usuário da scene Cadastro
+    string pass; //variável declarada para receber o valor de senha da scene Cadastro
+    public static string usuarioReceb, senhaReceb; //variáveis para receber o valor do imput de usuario1 e senha1
+    public InputField Usuario1, Senha1; //recebendo os textos dos Inputs da cena 
+    public Text loginIN; //caixa de texto para avisar a condição de erro
+    public Text loginVa; //caixa de texto para avisar a condição de válido
+
+    [SerializeField]
+    private GameObject lista;
+
+    public static int Index;
+
+    public void AutentificaLogin() //método para validar a entrada de dados registrados
+    {
+        user = Registrar.registerUser; //user recebe o valor do usuario registrado no script Registrar //forma de receber uma informação de uma cena para outra
+        pass = Registrar.registerPass; //pass recebe o valor da senha registrada no script Registrar 
+
+        Registrar.usuario.Contains(user);
+        Registrar.codigo.Contains(pass);
+
+        usuarioReceb = Usuario1.text; //fazendo com que minha variável receba o Input de usuario1
+        senhaReceb = Senha1.text; //fazendo com que minha variável receba o Input de senha1
+
+        foreach (string uso in Registrar.usuario)
+        {
+            if(uso.Equals(usuarioReceb)) 
+            {
+                Index = Registrar.usuario.IndexOf(uso);
+
+                if((Registrar.codigo[Index].Equals(senhaReceb)&& (usuarioReceb != "") && (senhaReceb != "")))
+                {
+                    loginIN.enabled = false; //não transmite o aviso
+                    StartCoroutine("Avisotempo"); //aciona o método de tempo
+                }
+                else
+                {
+                    loginIN.enabled = true; //condição para a caixa de texto aparecer 
+                    loginVa.enabled = false; //não transmite o aviso
+                
+                }
+
+            }
+        }
+
+        if((usuarioReceb=="Madu")&&(senhaReceb =="1234")){ //login ADM
+
+            lista.SetActive(true);
+            loginVa.enabled = true;
+        }
+
+    }
+
+    public void Controle()
+    {
+        SceneManager.LoadScene("Controle");
+    }
+
+    IEnumerator Avisotempo(){ //método para fazer um timer
+        loginVa.enabled = true; //condição para a caixa de texto aparecer
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene("Perfil"); //forma de puxar uma cena
+    }
+
+    public void registro() 
+    {
+        SceneManager.LoadScene("Cadastro"); //forma de puxar uma cena ao clicar no Button que vai receber esse método
+    }
+    public void Sair()
+    {
+        Application.Quit(); //forma de puxar uma cena ao clicar no Button que vai receber esse método
+    }
+}
